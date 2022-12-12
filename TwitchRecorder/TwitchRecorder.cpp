1b0273eacbc38af055c8121210cba53bc7cf35e3
Alexandre Martins <deadmaxfr@gmail.com>
2022-12-12 20:49:35 +0100

    Ajoutez des fichiers projet.

---
+++ TwitchRecorder/TwitchRecorder.cpp
@@ -0,0 +1,12 @@
+﻿// TwitchRecorder.cpp : définit le point d'entrée de l'application.
+//
+
+#include "TwitchRecorder.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
